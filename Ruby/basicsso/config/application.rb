18b3641086b4a19d460bda56e93d35ea6e3a4427
cloris <suheart@sohu.com>
2017-08-31 09:39:32 +0800

    delete project code

--- Ruby/basicsso/config/application.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'boot'
-require 'rails/all'
-
-Bundler.require(*Rails.groups)
-module EDUGraphAPIRuby
-  class Application < Rails::Application
-    config.eager_load_paths += %W(#{config.root}/lib)
-  end
-end
--- Ruby/basicsso/config/initializers/assets.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( 
-site.js
-)
